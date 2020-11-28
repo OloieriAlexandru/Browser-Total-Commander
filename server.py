9e235dca9d5fc531c9c34dfcd6b23b09b2506678
Alexandru Oloieri <alexoloieri2014@gmail.com>
2020-11-29 00:58:02 +0200

    Added web page structure v1

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template
 PORT = 3333
 
 app = Flask(__name__,
+            static_folder='public',
             template_folder='public')
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(port=PORT)
+  app.run(port=PORT)
